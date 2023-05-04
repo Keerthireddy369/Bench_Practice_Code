7bf27213330da2f122d0b1ba7bd2d10c7ddcdd30
Keerthi Reddy <115027344+Keerthireddy369@users.noreply.github.com>
2023-05-04 12:49:59 +0530

    Add files via upload

---
+++ 18-04-2023/C_Sharp_18-04-23_Basics/C_Sharp_18-04-23_Basics/ReverseOfString_With_ReverseMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C_Sharp_18_04_23_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PROGRAM FOR REVERSE OF A STRING");
+            Console.WriteLine("Enter any string:");
+            string value = Console.ReadLine();
+            char[] stringArray = value.ToCharArray();
+            Array.Reverse(stringArray);
+            string reversedArray = new string(stringArray);
+            Console.WriteLine($"Reversed String:{reversedArray}");
+
+
+        }
+    }
+}
---
+++ 18-04-2023/C_Sharp_18-04-23_Basics/C_Sharp_18-04-23_Basics/ReverseOfString_Without_Using_Reversemethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_18_04_23_Basics
+{
+    class ReverseOfString_Without_Using_Reversemethod
+    {
+        static void Main()
+        {
+            int i, j, l;
+            Console.WriteLine("PROGRAM FOR REVERSE OF A STRING");
+            Console.WriteLine("Enter any string:");
+            string value = Console.ReadLine();
+            char[] stringArray = value.ToCharArray();
+            l = value.Length;
+            
+            
+
+        }
+    }
+}

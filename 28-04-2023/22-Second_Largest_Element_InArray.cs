f09740c6a96fdc8c803a8ce60be417699b88b368
Keerthi Reddy <115027344+Keerthireddy369@users.noreply.github.com>
2023-05-04 12:58:24 +0530

    Add files via upload

---
+++ 28-04-2023/22-Second_Largest_Element_InArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_C_Sharp_26_04_23
+{
+    class Second_Largest_Element_InArray
+    {
+        static void Main()
+        {
+            int n;
+            Console.WriteLine("Enter the size of an array:");
+            n = Convert.ToInt32(Console.ReadLine());
+            int i;
+            int[] arr = new int[n]; 
+            Console.WriteLine("Enter elements into an Array:");
+            for (i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            Console.WriteLine($"The Second Largest Element In The Array Was : {arr[n-2]}");
+        }
+    }
+}
---
+++ 28-04-2023/23-Sorting_Without_Sort-and_Copying.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_C_Sharp_26_04_23
+{
+    class _23_Sorting_Without_Sort_and_Copying
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("Enter the array size:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            int[] arr1 = new int[n];
+
+            int i,j,temp;
+            Console.WriteLine("Enter elements into array:");
+            for (i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Copied Array Was:");
+            for (i = 0; i < arr.Length; i++)
+            {
+                arr1[i] = arr[i];
+
+               
+            }
+            foreach (var ch in arr1)
+            {
+                Console.Write(ch);
+            }
+
+            for (i = 1; i < arr.Length; i++)
+            {
+                for (j = 0; j < arr.Length - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp; 
+                    }
+                }
+            }
+            Console.WriteLine("Ascending order of an array was:");
+            foreach(var c in arr)
+            {
+                temp = c;
+                Console.WriteLine(c);
+            }
+            
+        }
+    }
+}

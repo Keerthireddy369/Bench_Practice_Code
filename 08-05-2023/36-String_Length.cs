b68da488d8618748730b0eea4da4c4f2bce5d9dd
Keerthi Reddy <115027344+Keerthireddy369@users.noreply.github.com>
2023-05-10 17:24:21 +0530

    Add files via upload

---
+++ 08-05-2023/36-String_Length.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_C_Sharp_26_04_23
+{
+    class _36_String_Length
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            int str1 = str.Length;
+            int len = 0;
+            for (int i = 1; i <= str1; i++)
+            {
+                len = i++;
+            }
+            Console.WriteLine("Length of the given string is " + len);
+        }
+    }
+}
---
+++ 08-05-2023/37-Removing_Spaces.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_C_Sharp_26_04_23
+{
+    class _37_Removing_Spaces
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            Console.WriteLine("After removing spaces");
+            string str1 = str.Replace(" ", "");
+            Console.WriteLine(str1);
+        }
+    }
+}
